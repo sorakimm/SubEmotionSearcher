239b6d619fdbac03d810d7c400834e55b89f4cfd
Sora <sorak17@gmail.com>
2018-09-28 00:16:46 +0900

    pagination update

--- app/searchsite/urls.py
+++ app/searchsite/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.main, name='main'),
     # url(r'^searchsite/(?P<emotion>\w+)$', views.search_list, name='search_list'),
-    url(r'^search/(?P<emotion>\w+)/$', views.emotion_search_list, name='search_list'),
-    url(r'^search/term/$', views.term_search_list, name='term_search_list'),
+    # url(r'^search/(?P<emotion>\w+)/$', views.emotion_search_list, name='search_list'),
+    # url(r'^search/term/$', views.term_search_list, name='term_search'),
+
+    url(r'^search/emotion/(?P<emotion>\w+)/$', views.emotion_search_list, name='emotion_search_list'),
+    url(r'^search/term/$', views.term_search_list, name='search_list'),
 ]
